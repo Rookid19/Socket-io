34a78fcefc4d0145292318e0ce7da40a6b13b838
Randy Odoom <randyodoom19@gmail.com>
2022-12-20 18:35:21 +0000

    receive

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3000");
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [lastPong, setLastPong] = useState(null);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setIsConnected(true);
+    });
+
+    socket.on("disconnect", () => {
+      setIsConnected(false);
+    });
+
+    socket.on("pong", () => {
+      setLastPong(new Date().toISOString());
+    });
+
+    socket.off("receive").on("receive", (data) => {
+      console.log(data);
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("pong");
+    };
+  }, []);
+
+  const sendPing = () => {
+    socket.emit("ping");
+  };
+
+  // //
+  // useEffect(() => {
+  // socket.on("receive", (data) => {
+  //   console.log(data);
+  // });
+  //   // return () => socket.emit("end");
+  // }, [socket]);
+
+  //
+  const send = () => {
+    socket.emit("chat message", message);
+  };
   return (
     <div className="App">
-    
+      <input value={message} onChange={(e) => setMessage(e.target.value)} />
+      <button onClick={send}>Send message</button>
+      <div>Yoo wassup</div>
+      <br />
+      <p>Connected: {"" + isConnected}</p>
+      <p>Last pong: {lastPong || "-"}</p>
+      <button onClick={sendPing}>Send ping</button>
     </div>
   );
 }
